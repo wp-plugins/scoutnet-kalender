c11c21c842f6e183635d635d6e233bc9e93e5530
derweed <derweed@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-20 14:26:36 +0000

    - Update Version 0.2.2 - WP3.9 compatibility
    git-svn-id: https://plugins.svn.wordpress.org/scoutnet-kalender/tags/0.2.2@898725 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/inline_kalender_list.php
@@ -0,0 +1,36 @@
+<?php
+/*
+	Scoutnet Kalender Template: WIDGET-EVENTS (default)
+	
+	Dir stehen hier alle Inhalte des Kalenders in einem Array zur Verfügung.
+	Z.B.:
+	    	<?php echo date('d. m. Y', $event->Start); ?>
+		<?php echo $event->Title; ?>
+		<?php echo $event->Author->get_full_name(); ?>
+		<?php var_dump($event); ?>
+*/ 	
+
+foreach($events as $event) { /* @var $event SN_Model_Event */
+?>
+	<h2><?php echo date('d.m.Y', $event->Start); ?></span>: <?php echo $event->Title; ?></h2>
+	
+	<p>
+		<?php echo $event->Description; ?>
+		<br />
+		eingetragen von: <?php echo $event->Author->get_full_name(); ?>
+		<br />
+		<?php
+		// Zeigt den Link nur an, wenn das Feld gefüllt ist
+		if (trim($event->URL)!="") {
+			// Zeigt den Link_Text (mit Link) nur an, wenn das Feld gefüllt ist
+			if (trim($event->URL_Text)!="") { ?>
+				Link: <a href="<?php echo $event->URL; ?>"><?php echo $event->URL_Text; ?></a>
+			<?php }
+				else { ?>
+				Link: <a href="<?php echo $event->URL; ?>"><?php echo $event->Title; ?></a>
+			<?php	}
+		}
+		?>
+	</p>
+	<br />
+<?php } ?>
